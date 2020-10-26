2eca4893bf82db2d8187a8ad695254238cd4a71b
Quynh <hp.quynh@yahoo.com>
2020-10-25 17:14:59 -0700

    more set-up

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
-app.set('view engine','ejs')
-app.set('views',__dirname + '/routes')
-app.set('layout','layouts/layout')
-app.use(express.static('public'))
-app.use(expressLayouts)
+const indexRouter=require('./routes/index.js')
 
+app.set('view engine','ejs')
+app.set('views',__dirname + '/views')
+app.set('layout','layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+app.use('/', indexRouter)
 app.listen(process.env.PORT || 3000)
