5cd732e41c6468cd194d00ae4722e981fa6c03b6
Quynh <hp.quynh@yahoo.com>
2020-12-31 21:31:42 -0800

    working on nodemailer

--- src/config/google-auth.js
+++ src/config/google-auth.js
@@ -1,10 +1,10 @@
 /* eslint-disable @typescript-eslint/camelcase */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { OAuth2Client, Credentials } from 'google-auth-library';
+const { OAuth2Client, Credentials } = require('google-auth-library');
 
 const scope = 'https://mail.google.com';
 
-export const credentials = {
+exports.credentials = {
   client_id: process.env.GOOGLE_CLIENT_ID || '',
   project_id: process.env.GOOGLE_PROJECT_ID || '',
   auth_uri: 'https://accounts.google.com/o/oauth2/auth',
@@ -14,7 +14,7 @@ export const credentials = {
   javascript_origins: ['http://localhost'],
 };
 
-export function getAuthorizeUrl(callback: (err: any, url: string) => any): void {
+exports.getAuthorizeUrl = (callback) => {
   const oauth2Client = new OAuth2Client(credentials.client_id, credentials.client_secret, credentials.redirect_uris[0]);
 
   const authUrl = oauth2Client.generateAuthUrl({
@@ -25,13 +25,13 @@ export function getAuthorizeUrl(callback: (err: any, url: string) => any): void 
   callback(null, authUrl);
 }
 
-export function getAccessToken(callback: (err: any, token?: Credentials | null) => any): void {
+exports.getAccessToken = (callback) => {
   const code = process.env.GOOGLE_AUTH_CODE || '';
   const oauth2Client = new OAuth2Client(credentials.client_id, credentials.client_secret, credentials.redirect_uris[0]);
   oauth2Client.getToken(code).then(callback);
 }
 
-export const tokens: Credentials = {
+exports.tokens = {
   access_token: process.env.GOOGLE_ACCESS_TOKEN || '',
   token_type: 'Bearer',
   refresh_token: process.env.GOOGLE_REFRESH_TOKEN || '',
